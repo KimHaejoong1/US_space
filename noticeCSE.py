2da8e914a1a1dc6c468e59768973cdeb4773e2cb
KimHaeJoong1 <hjkim2124@gmail.com>
2024-02-04 15:00:10 +0900

    modified:   noticeCSE.py / add comment

--- noticeCSE.py
+++ noticeCSE.py
@@ -4,8 +4,8 @@ from json import dumps
 
 
 def get_CSE_notice():
+    #학과 공지사항 파싱
     url = 'https://cse.pusan.ac.kr/cse/14651/subview.do'
-
     response = requests.get(url)
 
     notice_data = []
@@ -14,12 +14,14 @@ def get_CSE_notice():
         soup = BeautifulSoup(html, 'html.parser')
         tr = soup.find_all('tr')
 
+        # 중요공지 1개와 최근 공지사항 처리
         filtered_rows = [tr[1]]
         for row in tr:
             td_num = row.find('td', class_='_artclTdNum')
             if td_num and not td_num.find('span', class_='_artclNotice _artclNnotice'):
                 filtered_rows.append(row)
 
+        #필터링된 공지사항 처리
         for content in filtered_rows:
             title = content.find('strong').get_text()
             notice = {
