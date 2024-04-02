65240d5f2e4d0a310ba3e1b396e140e92c738704
KimHaeJoong1 <hjkim2124@gmail.com>
2024-04-02 11:43:32 +0900

    modified:   noticeSELON.py / error modified

--- noticeSELON.py
+++ noticeSELON.py
@@ -15,21 +15,25 @@ def get_SELON_notice():
     
     #노션 접속하기
     driver.get(url = 'https://pnucse2024.notion.site/pnucse2024/5df7c0d47d984f569928f8e98a05a7f7?v=169c894696944fd8b4a6daa2a10c177e')
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="notion-app"]/div/div[1]/div/div[1]/main/div/div[5]/div/div/div[1]/div[3]/div[2]/div/div/div[2]/div/div/a/span')))
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="notion-app"]/div/div[1]/div/div[1]/main/div/div/div[5]/div/div/div[1]/div[3]/div[2]')))
     
     #학생회 공지사항 처리
     notice_data = []
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    div = soup.find_all('div', class_="notion-selectable notion-page-block notion-collection-item")
-    for content in div:
-        notice = {
-            "title" : content.find_all('span')[0].get_text(),
-            "type" : content.find_all('span')[1].get_text(),
-            "status" : content.find_all('span')[2].get_text(), 
-        }
-        notice_data.append(notice)
     
+    #부모 div 찾기
+    parent_div = soup.find_all('div', class_="notion-selectable notion-page-block notion-collection-item")
+
+    if parent_div:
+        for div in parent_div[0:7]:
+            notice = {
+                "title" : div.find_all('span')[0].get_text(),
+                "type" : div.find_all('span')[1].get_text(),
+                "status" : div.find_all('span')[2].get_text(), 
+            }
+            notice_data.append(notice)
+
     return notice_data
 
 if __name__ == '__main__':
